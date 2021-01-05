536f0d4c08f7a1460d7ae5084ab53b1c6e0066e5
Kfir <74012686+kfiree@users.noreply.github.com>
2021-01-05 02:24:51 +0200

    tons of sh!t
    * updated all methods to work with new edges implementation.
    * add documentation to DiGraph.py

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -2,87 +2,251 @@ from GraphInterface import GraphInterface
 
 
 class DiGraph:
-
-
-    # TODO construct a graph than can easily make a Json like:
-    # {
-    #    "edges": [{src, weigh, dest}...]
-    #    "nodes": [{pos: (x, y, z), id}...]
-    # }
+    # TODO construct a graph than can easily make a Json like: {"edges": [{src, weigh, dest}...], "nodes": [{pos: (x, y, z), id}...]}
+    # TODO add raises to methods
 
     """
-        edges = [
-            from={src: {dest:w]],
-            to={dest: {src:w]]
-        ]
+      A class representing graph
+    ...
+
+    Attributes
+    ----------
+    nodes : dict
+        a formatted dictionary contains all graph's nodes
+
+    edges : dict
+        a formatted dictionary contains all graph's edges
+
+    Methods
+    -------
+    v_size()
+        Returns the number of vertices in this graph
+
+    e_size()
+        Returns the number of edges in this graph
+
+    get_all_v()
+        Returns a dictionary of all the nodes in the Graph.
+        node is represented using a pair (node_id, node_data)
+
+    all_in_edges_of_node(int: node_id)
+        return a dictionary of all the node's edges,
+        edge is represented using a pair (destination, weight)
+
+    all_out_edges_of_node(int: node_id)
+        return a dictionary of all the edges that pointing at current node,
+        each node is represented using a pair(source, weight)
+
+    get_mc()
+        Returns the current version of this graph,
+        on every change in the graph state - the MC (mode counter) increased
+
+    add_edge(int: source,int: destination, float: weight)
+        Adds an edge to the graph.
+
+    add_node(node_id: int, pos: tuple = None)
+        Adds a node to the graph.
+
+    remove_node(self, node_id: int)
+        Removes a node from the graph.
+
+    remove_edge(self, node_id1: int, node_id2: int)
+        Removes an edge from the graph.
+
     """
-    #TODO add comment for node structor
+
     def __init__(self):
+        """
+        Parameters
+        ----------
+            nodes : {
+                node_id:{
+                    "tag": int,
+                    "info": "",
+                    "pos":(x,y,z),
+                    "prev": int
+                }
+            }
+
+            edges = {
+                From: {src: {dest:w}},
+                To: {dest: {src:w}}
+            }
+        """
+        self.edges = {"From": {}, "To": {}}  # {src_id: {dest, weight}}
         self.nodes = {}  # {node_id, node_data(like Gson)}
-        self.edges = {}  # {src_id: {dest, weight}}
         self.mc = 0
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
-        if not self.nodes.__contains__(node_id):
-            self.nodes[node_id] = {"neighbors": {}, "neighbor of": {}, "tag": 0, "info": "", "pos": tuple, "pre": int}
-            self.edges[node_id] = {}
+        """
+        Adds a node to the graph.
+
+        Parameters
+        ----------
+        node_id: int
+            node unique key
+        pos: tuple, optional
+            node's position (default None)
+
+        Returns
+        -------
+        bool
+            a flag used to indicate if node addition was successful
+        """
+
+        if node_id not in self.nodes:
+            self.nodes[node_id] = {"tag": 0, "info": "", "pos": tuple, "prev": int}
+            self.edges["From"][node_id] = {}
+            self.edges["To"][node_id] = {}
             self.mc += 1
             return True
         else:
             return False
 
     def v_size(self) -> int:
-        return self.nodes.__sizeof__()
+        """"
+        Returns
+        -------
+        int
+            the number of vertices this graph contains
+
+        """
+        return len(self.nodes)
 
     def e_size(self) -> int:
-        return self.edges.__sizeof__()
+        """"
+               Returns
+               -------
+               int
+                   the number of edges this graph contains
+
+       """
+        return len(self.edges)
 
     def get_all_v(self) -> dict:
+        """"
+               Returns
+               -------
+               dict
+                   a dictionary of vertices this graph contains.
+                   node is represented using a pair (node_id, node_data)
+
+               """
         return self.nodes
 
-    def all_in_edges_of_node(self, id1: int) -> dict:
-        if self.nodes.keys().__contains__(id1):
-            return self.nodes[id1].get("neighbor of")
+    def all_in_edges_of_node(self, node_id: int) -> dict:
+        """"
+            Parameters
+            ----------
+            node_id: int
+                node unique key
 
+            Returns
+            -------
+            dict
+                a dictionary of all the node's edges,
+                edge is represented using a pair (destination, weight)
+
+       """
+        if node_id in self.nodes:
+            return self.edges["To"][node_id]
         else:
             return None
 
-    def all_out_edges_of_node(self, id1: int) -> dict:
-        if self.nodes.keys().__contains__(id1):
-            return self.nodes[id1].get("neighbors")
+    def all_out_edges_of_node(self, node_id: int) -> dict:
+        """"
+            Parameters
+            ----------
+            node_id: int
+                node unique key
+
+            Returns
+            -------
+            dict
+                return a dictionary of all the edges that pointing at current node,
+                each node is represented using a pair(source, weight)
+
+       """
+        if node_id in self.nodes:
+            return self.edges["From"][node_id]
         else:
             return None
 
     def get_mc(self) -> int:
+        """"
+            Returns
+            -------
+            int
+                Returns the current version of this graph
+        """
         return self.mc
 
-    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        if self.nodes.__contains__(id1) and self.nodes.__contains__(id2):
-            self.edges[id1][id2] = weight
-            self.nodes[id1].get("neighbors")[id2] = weight
-            self.nodes[id2].get("neighbor of")[id1] = weight
+    def add_edge(self, src: int, dest: int, weight: float) -> bool:
+        """"
+            Parameters
+            ----------
+            src: int
+                edge's source node
+            dest: int
+                edge's destination node
+
+            Returns
+            -------
+            bool
+                a flag used to indicate if edge addition was successful
+
+       """
+        if {src, dest} <= self.nodes.keys():
+            self.edges["From"][src].update({dest: weight})
+            self.edges["To"][dest].update({src: weight})
+
             self.mc += 1
             return True
         else:
             return False
 
     def remove_node(self, node_id: int) -> bool:
-        if self.nodes.keys().__contains__(node_id):
-            for node in self.all_in_edges_of_node(node_id).keys():
-                self.remove_edge(node, node_id)
+        """"
+            Parameters
+            ----------
+            node_id: int
+                node's unique key
 
-            self.mc += self.all_out_edges_of_node(node_id).__sizeof__()
-            del self.edges[node_id]
+            Returns
+            -------
+            bool
+                a flag used to indicate if node's deletion was successful
+
+       """
+        if node_id in self.nodes:
             del self.nodes[node_id]
+            removedNodeE = self.edges["From"].pop(node_id);
+            for neighbor_id in removedNodeE:
+                del self.edges["From"][neighbor_id]
+
+            self.mc += 1
             return True
         else:
             return False
 
-    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        if self.nodes.keys().__contains__(node_id1) and self.nodes.keys().__contains__(node_id2):
-            del self.edges[node_id1][node_id2]
-            del self.nodes[node_id1].get("neighbors")[node_id2]
-            del self.nodes[node_id2].get("neighbor of")[node_id1]
+    def remove_edge(self, src: int, dest: int) -> bool:
+        """"
+            Parameters
+            ----------
+            src: int
+                edge's source node
+            dest: int
+                edge's destination node
+
+            Returns
+            -------
+            bool
+                a flag used to indicate if edge's deletion was successful
+
+       """
+        if {src, dest} <= self.nodes.keys():
+            del self.edges["From"][src][dest]
+            del self.edges["To"][dest][src]
             self.mc += 1
             return True
         else:
